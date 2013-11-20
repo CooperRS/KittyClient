9ea9987daaf6ee2c2ed653e03835906b54fcdf4b
CooperRS <CooperRS@yasit.net>
2013-11-20 19:49:31 +0100

    Prepared project for Travis

---
+++ KittyClientTests/KCKittyClientTests.m
@@ -0,0 +1,20 @@
+//
+//  KCKittyClientTests.m
+//  KittyClient
+//
+//  Created by Roland Moers on 20.11.13.
+//  Copyright (c) 2013 Simon Jakubowski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KCKittyClientTests : XCTestCase
+
+@end
+
+@implementation KCKittyClientTests
+
+- (void)testExample {
+}
+
+@end
